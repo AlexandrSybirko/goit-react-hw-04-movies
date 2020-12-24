0995da61ba52f3553cdd78a0f91dbc24d16cb302
AlexandrSybirko <63520677+AlexandrSybirko@users.noreply.github.com>
2020-12-24 12:41:09 +0200

    Делает Новигацию

--- src/service/movies-api.js
+++ src/service/movies-api.js
@@ -1,2 +1,23 @@
 const BASE_URL = 'https://api.themoviedb.org/3';
 const API_KEY = '2c8047fe24212bec24c8e99b54a108ca';
+
+
+
+function fetchTrendingMoves() {
+
+  return fetch(
+    `${BASE_URL}/trending/all/day?api_key=${API_KEY}`,
+  ).then(response => {
+    if (response.ok) {
+      return response.json();
+    }
+
+    return Promise.reject(new Error('No response from server'));
+  });
+}
+
+const api = {
+  fetchTrendingMoves,
+}
+
+export default api;
